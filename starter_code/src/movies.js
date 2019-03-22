205ca3cccbf7df75eaf18dc57a40026516f527e9
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-03-22 23:17:49 +0100

    Start the excercices

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,31 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
-
-// Get the average of all rates with 2 decimals 
-
+// Get the average of all rates with 2 decimals
+function ratesAverage(movies) {
+  let sum = 0;
+  movies.forEach(function(movie, i, array) {
+    sum += Number(movie.rate);
+  });
+  return sum / movies.length;
+}
 
 // Get the average of Drama Movies
-
-
+function dramaMoviesRate(movies) {
+  let sum = [];
+  movies.forEach(function(movie, i, array) {
+    if (movie.genre.includes("Drama")) {
+      sum.push(i);
+    }
+  });
+  console.log(sum);
+  return sum;
+}
+dramaMoviesRate(movies);
 // Order by time duration, in growing order
-
 
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
